4f8d8d92604c07c365f702f05ae773a559513221
srinathram17 <srinathram17@gmail.com>
2022-09-09 21:22:09 +0530

    initial String project

---
+++ src/org/test/stringgg/Stringgg.java
@@ -0,0 +1,38 @@
+package org.test.stringgg;
+
+public class Stringgg {
+
+
+	public static void main(String[] args) {
+
+		//		Length is nothing but find the number of size of the word
+
+		String s = "GreensTechnology";
+		String s1 = "SeleniumAutomationtool";
+		String s2= "velmurugan";
+		String s3= "j a v a p r o g r a m";
+		String s4= "9095484678";
+
+		int length = s.length();
+		System.out.println(length);
+
+		System.out.println(s1.length());
+		System.out.println(s2.length());
+		System.out.println(s3.length());
+		System.out.println(s4.length());
+		
+		System.out.println(s.charAt(9));
+		System.out.println(s1.charAt(11));
+		System.out.println(s2.charAt(4));
+		System.out.println(s3.charAt(8));
+		System.out.println(s4.charAt(8));
+
+
+
+
+
+
+
+	}
+
+}
